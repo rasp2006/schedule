e1f14c036d3314d4e67de5e7f2ca055045665cf6
rasp2006 <78358571+rasp2006@users.noreply.github.com>
2025-04-18 09:24:00 +0500

    changes

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 cd ./ScheduleURPET
 python3 -m venv venv
 ./venv/bin/activate
-pip install -r schedule_service/requirements.txt
-uvicorn schedule_service.main:app &
+pip install -r ./requirements.txt
+uvicorn schedule_service_2.main:app --host 0.0.0.0 --port 80 &
